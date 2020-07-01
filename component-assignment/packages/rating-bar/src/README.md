d04c99e1ce54c46cbf4050c52a3d2d1f0dc4bff0
trunaldeshmukh <tdeshmukh@tavisca.com>
2020-07-01 16:14:15 +0530

    rating bar changes

--- component-assignment/packages/rating-bar/src/README.md
+++ component-assignment/packages/rating-bar/src/README.md
@@ -1,1 +1,35 @@
-# orxe-hello
+## Usage
+
+### Angular / javascript
+
+_Default_
+
+```html
+<orxe-rating-bar> </orxe-rating-bar>
+```
+
+_Linear Rating Bar with initial rating_
+
+```html
+<orxe-rating-bar type="linear" rating="60"></orxe-rating-bar>
+```
+
+_Donut Rating Bar with initial rating_
+
+```html
+<orxe-rating-bar type="donut" rating="30"></orxe-rating-bar>
+```
+
+## Methods
+
+| Method Name | Return Type | Access Modifier | Description                                                                   |
+| ----------- | ----------- | --------------- | ----------------------------------------------------------------------------- |
+| `getRating` | `number`    | public          | This method converts the rating based on the scale of 100 to the scale of 10. |
+
+## Properties
+
+| Property | Attribute | Description                                    | Type     | Default  |
+| -------- | --------- | ---------------------------------------------- | -------- | -------- |
+| `type`   | `type`    | Used to display the type of rating bar.        | `string` | `linear` |
+| `rating` | `rating`  | Used to pass a value/rating to the rating bar. | `number` | `0`      |
+| `label`  | `label`   | To provide a label for the linear rating bar.  | `string` |          |
--- component-assignment/packages/rating-bar/src/orxe-rating-bar.ts
+++ component-assignment/packages/rating-bar/src/orxe-rating-bar.ts
@@ -1,15 +1,131 @@
-import { html, customElement, LitElement } from 'lit-element';
+import { html, customElement, LitElement, property, TemplateResult } from 'lit-element';
 import styles from './rating-bar-css';
+import {styleMap} from 'lit-html/directives/style-map.js';
 
 @customElement('orxe-rating-bar')
 export default class OrxeRatingBar extends LitElement {
   /**
    * Implement `render` to define a template for button element.
    */
+  @property({ type: String, reflect: true })
+  type = 'donut';
+
+  @property({ type: Number, reflect: true })
+  rating = 50;
+
+  @property({ type: String, reflect: true })
+  label = 'Rating';
+
+  private actualRating = 0;
+
   render() {
+    this.actualRating = this.getRating();
     return html`
-      <p>Hello, Welcome to lit Elements</p>
+      ${this._renderRatingBar()}
     `;
+  }
+  getRating(): number {
+    return !this.rating
+      ? 0
+      : this.rating > 100
+        ? 10
+        : this.rating < 0
+          ? 0
+          : Math.floor(this.rating) / 10;
+  }
+
+  private _renderRatingBar(): TemplateResult {
+    if (this.type == 'linear') {
+      return html`
+        <div class="rating-bar-container" data-testid="linear-rating-bar">
+          <div
+            data-testid="linear-indicator"
+            style=${styleMap(this._showProgress())}
+            class="${this._getColurClass()} rating-bar">
+          </div>
+        </div>
+        <div class="rating-bar-info">
+          <span>${this.label}</span>
+          <span data-testid="linear-rating-value">${this.actualRating}</span>
+        </div>
+      `;
+    }
+    else {
+      return html`
+      <svg
+      data-testid="donut-rating-bar"
+      aria-hidden="true"
+      class="donut"
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+    >
+      <circle cx="20" cy="20" r="18" class="donut-container" />
+      <circle
+        data-testid="donut-track-indicator"
+        cx="20"
+        cy="20"
+        r="18"
+        style=${styleMap(this._showProgress())}
+        class="donut-indicator ${this._getdonutClasses()}"
+      />
+    </svg>
+    <div data-testid="donut-rating" class="donut-info">${this.actualRating}</div>`;
+
+    }
+
+  }
+  private _getColurClass(): string {
+    if (this.actualRating >= 1 && this.actualRating < 3) {
+      return 'rating--bad';
+    } else if (this.actualRating >= 3 && this.actualRating < 5) {
+      return 'rating--poor';
+    } else if (this.actualRating >= 5 && this.actualRating < 7) {
+      return 'rating--average';
+    } else if (this.actualRating >= 7 && this.actualRating < 8.5) {
+      return 'rating--great';
+    } else if (this.actualRating >= 8.5) {
+      return 'rating--excellent';
+    } else {
+      return '';
+    }
+  }
+  private _showProgress(): Record<string, string> {
+    const progress = {};
+    if (this.type == 'donut') {
+      if (this.rating > 100) {
+        progress['stroke-dashoffset'] = 0;
+      } else if (this.rating < 0) {
+        progress['stroke-dashoffset'] = 113.04;
+      } else {
+        progress['stroke-dashoffset'] = 113.04 * (1 - this.rating / 100);
+      }
+      return progress;
+    } else {
+      if (this.rating > 100) {
+        progress['width'] = '100%';
+      } else if (this.rating < 0) {
+        progress['width'] = '0%';
+      } else {
+        progress['width'] = this.rating + '%';
+      }
+      return progress;
+    }
+  }
+  private _getdonutClasses(): string {
+    if (this.actualRating >= 1 && this.actualRating < 3) {
+      return `donut-bad`;
+    } else if (this.actualRating >= 3 && this.actualRating < 5) {
+      return `donut-poor`;
+    } else if (this.actualRating >= 5 && this.actualRating < 7) {
+      return `donut-average`;
+    } else if (this.actualRating >= 7 && this.actualRating < 8.5) {
+      return `donut-great`;
+    } else if (this.actualRating >= 8.5) {
+      return `donut-excellent`;
+    } else {
+      return '';
+    }
   }
 
   /**
